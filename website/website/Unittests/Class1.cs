97799902c97659216e7fec850f94e83b40003e36
Jurian28 <120514288+Jurian28@users.noreply.github.com>
2026-03-10 16:52:54 +0100

    dotnet project aangepast

---
+++ website/website/Unittests/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Unittests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
