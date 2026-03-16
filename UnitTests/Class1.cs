ab3e69a27ee942538691bb9450aae950f32bb4ef
Jurian28 <120514288+Jurian28@users.noreply.github.com>
2026-03-16 22:42:48 +0100

    unit test project toegevoegd

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 5;
+            Assert.IsTrue(x == 5);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int x = 4;
+            Assert.IsTrue(x == 5);
+        }
+    }
+}
