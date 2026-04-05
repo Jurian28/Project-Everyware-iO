8a8345a4018348f609a6c41b58b4b62915be3413
DouweP <dt.vanderploeg@student.avans.nl>
2026-04-05 22:49:30 +0200

    DTO en form toegevoegd

---
+++ SharedClassLibrary/DTOs/Sessions/CUSessionDTO.cs
@@ -0,0 +1,11 @@
+public class CUSessionDTO
+{
+    public SessionDTO? session { get; set; }
+
+    // has: RoomId, RoomLabel, Capacity
+    public List<SessionRoomDTO> AvailableRooms { get; set; } = new List<SessionRoomDTO>();
+    // has: EventId, Title
+    public List<SessionTagDTO> AvailableTags { get; set; } = new List<SessionTagDTO>();
+    // has: SpeakerId, Name
+    public List<SessionSpeakerDTO> AvailableSpeakers { get; set; } = new List<SessionSpeakerDTO>();
+}
