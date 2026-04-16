470540ec487e0a5a2502b67f3d5995e2baa2f045
NLYanick <yanick.plaggenmarsch@gmail.com>
2026-04-16 17:01:34 +0200

    Toast for publish event

--- Back-office/wwwroot/js/events.js
+++ Back-office/wwwroot/js/events.js
@@ -1,17 +1,25 @@
-﻿// # Toast
+﻿
 let toast = document.getElementById('event-toast');
 if (toast) {
     setTimeout(closeToast, 4000);
 }
 
+const toastMessage = sessionStorage.getItem('eventToastMessage');
+const toastType = sessionStorage.getItem('eventToastType');
+if (toastMessage && toastType) {
+    editToast(toastMessage, toastType);
+
+    sessionStorage.removeItem('eventToastMessage');
+    sessionStorage.removeItem('eventToastType');
+}
+
 function closeToast() {
     if (toast) {
-        toast.style.display = 'none';
+        toast.classList.add('d-none');
         toast.classList.remove('d-flex');
     }
 }
 
-// # Publish
 async function togglePublish(eventId, published) {
     const apiBaseUrl = document.getElementById('api-url').value;
     const url = `${apiBaseUrl}/api/event/${eventId}/publish`;
@@ -31,9 +39,26 @@ async function togglePublish(eventId, published) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
+        sessionStorage.setItem('eventToastMessage', `Successfully ${published ? 'Published' : 'Unpublished'}!`);
+        sessionStorage.setItem('eventToastType', 'success');
+
         window.location.reload();
     } catch (error) {
         console.error('Error:', error);
+        editToast("Failed to publish event", "danger");
         throw error;
+    }
+}
+
+function editToast(message, type) {
+    if (toast) {
+        toast.querySelector('p').textContent = message;
+
+        toast.classList.remove('d-none');
+        toast.classList.add('d-flex');
+        toast.classList.remove('bg-success', 'bg-danger', 'bg-');
+        toast.classList.add(`bg-${type}`);
+
+        setTimeout(closeToast, 4000);
     }
 }
