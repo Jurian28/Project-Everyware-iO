41a3d600bc6e0e8e2cc8fcd9e64a04c556ab02a0
MatthijsCr <msj.cromwijk@student.avans.nl>
2026-05-14 13:15:41 +0200

    api can send SpotsData

---
+++ DatabaseApi/Services/Interfaces/ISessionService.cs
@@ -0,0 +1,8 @@
+﻿using SharedClassLibrary.DTOs.Sessions;
+
+namespace DatabaseApi.Services.Interfaces;
+
+public interface ISessionService
+{
+    public Task<List<SessionSpotsDTO>> ReturnSessionSpotsData(int eventId, int? sessionId);
+}
---
+++ DatabaseApi/Services/SessionService.cs
@@ -0,0 +1,37 @@
+﻿using DatabaseApi.Models;
+using DatabaseApi.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using SharedClassLibrary.DTOs.Sessions;
+
+namespace DatabaseApi.Services;
+
+public class SessionService : ISessionService
+{
+    private readonly ApplicationDbContext _context;
+
+    public SessionService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<SessionSpotsDTO>> ReturnSessionSpotsData(int eventId, int? sessionId)
+    {
+        var query = _context.Sessions
+        .Include(s => s.Room)
+        .Where(s => s.IdEvent == eventId)
+        .AsQueryable();
+
+        if (sessionId != null)
+        {
+            query = query.Where(s => s.IdSession == sessionId);
+        }
+
+        return await query.Select(s => new SessionSpotsDTO
+        {
+            SessionId = s.IdSession,
+            TotalSpots = s.Room.Capacity,
+            FilledSpots = s.RegisteredUsers.Count(o => o.InWaitingList == false),
+            SpotsInWaitingList = s.RegisteredUsers.Count(o => o.InWaitingList == true),
+            IsPlenarySession = s.Plenary
+        }).ToListAsync();
+    }
+}
