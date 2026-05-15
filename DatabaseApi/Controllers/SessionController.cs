8d96c2e051aef984a4f0d276aad6356ae04cf66e
Jordi Keijzers <jordikeijzers@gmail.com>
2026-05-15 09:32:33 +0200

    Add api to register for a session and cancel registration

--- DatabaseApi/Controllers/SessionController.cs
+++ DatabaseApi/Controllers/SessionController.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using DatabaseApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SharedClassLibrary.DTOs.Rooms;
@@ -13,12 +14,15 @@ namespace DatabaseApi.Controllers
     public class SessionController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<SessionController> _logger;
+        
         private readonly string noRoomErrorMessage = "Geen gekoppelde kamer";
         private readonly string noSpeakerErrorMessage = "Geen gekoppelde spreker";
 
-        public SessionController(ApplicationDbContext context)
+        public SessionController(ApplicationDbContext context, ILogger<SessionController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -222,6 +226,149 @@ namespace DatabaseApi.Controllers
             {
                 return StatusCode(500);
             }
+        }
+
+        [HttpPost("{sessionId}/register")]
+        [Authorize]
+        public async Task<IActionResult> RegisterForSession(int eventId, int sessionId, bool overrideSessions = false)
+        {
+            User? user = _context.Users
+                .Include(user => user.Events)
+                .FirstOrDefault(user => user.UserName == User.Identity!.Name);
+
+            if (user == null) return BadRequest("User not found");
+
+            User_has_Session? existingRegistration = await _context.User_has_Sessions
+                .Where(userHasSession => userHasSession.IdSession == sessionId)
+                .Where(userHasSession => userHasSession.IdUser == user.Id)
+                .FirstOrDefaultAsync();
+
+            if (existingRegistration != null) return BadRequest("You have already registered for this session.");
+
+            Session? session = await _context.Sessions
+                .Include(session => session.RegisteredUsers)
+                .Include(session => session.Room)
+                .FirstOrDefaultAsync(session => session.IdSession == sessionId);
+
+            if (session == null) return NotFound("The session you tried to register for does not exist.");
+
+            List<User_has_Session> conflictingSessions = await _context.User_has_Sessions
+                .Include(userHasSession => userHasSession.Session)
+                .Where(userHasSession => userHasSession.IdUser == user.Id)
+                .Where(userHasSession => userHasSession.IdSession != session.IdSession)
+                .Where(userHasSession => userHasSession.Session.StartTime < session.EndTime && session.StartTime < userHasSession.Session.EndTime)
+                .ToListAsync();
+
+            if (conflictingSessions.Count > 0)
+            {
+                if (overrideSessions)
+                {
+                    _context.User_has_Sessions.RemoveRange(conflictingSessions);
+                }
+                else
+                {
+                    return BadRequest(new
+                    {
+                        Success = false,
+                        Data = new
+                        {
+                            Session = new
+                            {
+                                Id = session.IdSession,
+                                session.Title,
+                                session.StartTime,
+                                session.EndTime,
+                            },
+                            ConflictingSessions = conflictingSessions.Select(conflictingSession => new
+                            {
+                                Id = conflictingSession.IdSession,
+                                conflictingSession.Session.Title,
+                                conflictingSession.Session.StartTime,
+                                conflictingSession.Session.EndTime
+                            })
+                        },
+                        Error = "You are already registered for an event on during that time."
+                    });
+                }
+            }
+
+            int currentRegistrationCount = session.RegisteredUsers.Count;
+            int roomCapacity = session.Room.Capacity;
+            bool registerInQueue = currentRegistrationCount >= roomCapacity;
+
+            _context.User_has_Sessions.Add(new User_has_Session
+            {
+                IdSession = session.IdSession,
+                IdUser = user.Id,
+                InWaitingList = registerInQueue,
+                JoinedDate = DateTime.UtcNow
+            });
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError("Failed to save new registration: {exception}", exception);
+                return StatusCode(500);
+            }
+
+            return StatusCode(201, new
+            {
+                Success = true,
+                Data = (object?)null,
+                Error = (object?)null,
+                Message = registerInQueue
+                    ? "This session is full. Your registration is placed in the queue."
+                    : "You have registered yourself for this session."
+            });
+        }
+
+        [HttpPost("{sessionId}/cancel-registration")]
+        [Authorize]
+        public async Task<IActionResult> CancelRegistration(int eventId, int sessionId)
+        {
+            Session? session = await _context.Sessions
+                .Include(session => session.RegisteredUsers)
+                .Include(session => session.Room)
+                .FirstOrDefaultAsync(session => session.IdSession == sessionId);
+            User? user = _context.Users
+                .Include(user => user.Events)
+                .FirstOrDefault(user => user.UserName == User.Identity!.Name);
+
+            if (session == null || user == null) return BadRequest("User or session not found");
+
+            User_has_Session? registration = session.RegisteredUsers
+                .FirstOrDefault(registration => registration.IdUser == user.Id && registration.IdSession == session.IdSession);
+
+            if (registration == null) return BadRequest("You do not have a registration for that session");
+
+            _context.User_has_Sessions.Remove(registration);
+
+            List<User_has_Session> queuedRegistrations = session.RegisteredUsers
+                .Where(registeredUser => registeredUser.InWaitingList)
+                .Where(registeredUser => registeredUser.IdUser != user.Id)
+                .OrderByDescending(registeredUser => registeredUser.JoinedDate)
+                .ToList();
+
+            if (queuedRegistrations.Count > 0 && session.EndTime > DateTime.UtcNow)
+            {
+                User_has_Session oldestRegistration = queuedRegistrations.First();
+                oldestRegistration.InWaitingList = false;
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError("Failed to remove registration: {exception}", exception);
+                return StatusCode(500);
+            }
+
+            return Ok();
         }
     }
 }
