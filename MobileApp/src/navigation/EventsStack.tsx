d31aed60c0362b00b68fbe926418c50fe693ab85
NLYanick <yanick.plaggenmarsch@gmail.com>
2026-06-02 14:18:40 +0200

    Fixed navigation (hopefully didn't break things)

--- MobileApp/src/navigation/EventsStack.tsx
+++ MobileApp/src/navigation/EventsStack.tsx
@@ -1,7 +1,9 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import EventsOverview from '../pages/events/Overview';
 import EventPage from '../pages/events/Event';
-import EventScheduleStack from './EventScheduleStack';
+import EventSchedule from '../pages/events/EventSchedule';
+import SessionViewPage from '../pages/sessions/session';
+import QRCodePage from '../pages/sessions/QRCodePage';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +12,9 @@ export default function EventsStack() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="EventsOverview" component={EventsOverview} />
       <Stack.Screen name="Event" component={EventPage} />
-      <Stack.Screen name="EventSchedule" component={EventScheduleStack} />
+      <Stack.Screen name="EventSchedule" component={EventSchedule} />
+      <Stack.Screen name="SessionView" component={SessionViewPage} />
+      <Stack.Screen name="SessionQRCode" component={QRCodePage} />
     </Stack.Navigator>
   );
 }
