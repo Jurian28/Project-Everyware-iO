4974ae77a707baa921d9e9d20e3e71310afd8175
NLYanick <yanick.plaggenmarsch@gmail.com>
2026-06-04 17:28:41 +0200

    Also backend

--- DatabaseApi/Controllers/AttendanceController.cs
+++ DatabaseApi/Controllers/AttendanceController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using DatabaseApi.DTOs;
 using System.Net;
+using DatabaseApi.Services;
 
 namespace DatabaseApi.Controllers
 {
@@ -17,32 +18,37 @@ namespace DatabaseApi.Controllers
     {
         private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
         private readonly IWebHostEnvironment _environment = environment;
+        private readonly SessionRegistrationService _sessionRegistrationService = new(applicationDbContext);
 
         /// <summary>
-        /// Gets a paginated list of events, ordered by start date.
+        /// Checks if a user is attending a session and adds them to the attendance list if they are not already attending.
         /// </summary>
         [HttpPost]
         public async Task<IActionResult> CheckAttendance([FromRoute] int sessionId, [FromBody] string userId)
         {
             try
             {
-                if (string.IsNullOrEmpty(userId))
-                {
-                    return Unauthorized(ApiResponse<Object>.Fail("UserId is required"));
-                }
+                if (string.IsNullOrEmpty(userId)) return Unauthorized(ApiResponse<Object>.Fail("UserId is required"));
+                
+                if (sessionId <= 0) return BadRequest(ApiResponse<Object>.Fail("Invalid SessionId"));
+                
+                User? user = _applicationDbContext.Users
+                    .Include(user => user.Events)
+                    .FirstOrDefault(user => user.UserName == User.Identity!.Name);
+                if (user == null) return BadRequest(ApiResponse<object>.Fail("User not found"));
 
-                if (sessionId <= 0)
-                {
-                    return BadRequest(ApiResponse<Object>.Fail("Invalid SessionId"));
-                }
+                Session? session = await _sessionRegistrationService.GetSession(sessionId);
+                if (session == null) return NotFound(ApiResponse<object>.Fail("The session you tried to register for does not exist."));
 
-                var attendance = await _applicationDbContext.SessionAttendances
+                User_has_Session? existingRegistration = await _sessionRegistrationService.GetExistingRegistration(user, session);
+                if (existingRegistration == null) return BadRequest(ApiResponse<object>.Fail("You are not registered for this session"));
+
+                SessionAttendance? attendance = await _applicationDbContext.SessionAttendances
                     .FirstOrDefaultAsync(sa => sa.IdSession == sessionId && sa.UserId == userId);
 
                 if (attendance != null)
-                {
                     return StatusCode(409, ApiResponse<Object>.Fail("User is already attending this session"));
-                }
+                
                 else
                 {
                     await _applicationDbContext.SessionAttendances.AddAsync(new SessionAttendance
