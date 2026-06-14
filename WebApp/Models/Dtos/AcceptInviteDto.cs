a931ab885aa36787cf0af4714df3325b93cac7b7
DouweP <dt.vanderploeg@student.avans.nl>
2026-06-14 20:19:44 +0200

    old web app removed

--- WebApp/Models/Dtos/AcceptInviteDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApp.Models.Dtos;
-
-public class AcceptInviteDto
-{
-    public required bool Success { get; set; }
-    public required object? Data { get; set; }
-    public required string? Error { get; set; }
-}
--- WebApp/Models/ViewModels/AcceptInviteViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApp.Models.ViewModels;
-
-public class AcceptInviteViewModel
-{
-    public required bool Success { get; set; }
-
-    public required string Message { get; set; }
-}
